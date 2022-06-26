fcfb8868777617abb3f17a420776badc05821e58
l3on3l <fabianvera045@gmail.com>
2022-06-26 12:40:27 -0400

    add codes dir

---
+++ codes/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int suma (int a, int b) {
+	return a+b;
+}
+
+int main() {
+    int i;
+    i = 0;
+    
+    for (i = 0; i <10; i++)
+        i++;
+    
+    suma(4,5);
+    return 0;
+  }
